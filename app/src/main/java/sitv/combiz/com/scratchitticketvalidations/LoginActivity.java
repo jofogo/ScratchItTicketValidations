a504822e053ab42b3ca69fae2635c57a49c06d73
John Forrest Go <go_jfd@yahoo.com>
2018-08-23 00:17:11 +0800

    v1.0.1 ------ - Finished screens for the ff: -- Login -- Settings -- Ticket List Management -- Upload

--- app/src/main/java/sitv/combiz/com/scratchitticketvalidations/LoginActivity.java
+++ app/src/main/java/sitv/combiz/com/scratchitticketvalidations/LoginActivity.java
@@ -2,6 +2,8 @@ package sitv.combiz.com.scratchitticketvalidations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //Menu launcher
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_loggedout, menu);
+        return true;
     }
 }
--- app/src/main/java/sitv/combiz/com/scratchitticketvalidations/UploadActivity.java
+++ app/src/main/java/sitv/combiz/com/scratchitticketvalidations/UploadActivity.java
@@ -1,16 +1,13 @@
 package sitv.combiz.com.scratchitticketvalidations;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent activityLogin = new Intent(this, UploadActivity.class);
-        startActivity(activityLogin);
+        setContentView(R.layout.activity_upload);
     }
 }
